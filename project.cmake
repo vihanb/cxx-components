9792a518dac72ded5f675fc0fdabf22517f20726
Vihan <vihan+github@vihan.org>
2023-01-14 15:37:58 -0800

    Add homekit-sdk to gitmodules
    Signed-off-by: Vihan <vihan+github@vihan.org>

---
+++ project.cmake
@@ -0,0 +1,4 @@
+list(APPEND EXTRA_COMPONENT_DIRS esp-idf-components/ esp-idf-components/esp-homekit-sdk/components/ esp-idf-components/esp-homekit-sdk/components/homekit/)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
